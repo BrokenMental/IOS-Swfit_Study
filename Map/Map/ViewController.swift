54cc42d567be33f85e1a28f300274444f97b481a
407-24 <commontoday@naver.com>
2017-11-15 19:02:28 +0900

    Map 완성 커밋

--- Map/Map/ViewController.swift
+++ Map/Map/ViewController.swift
@@ -33,11 +33,21 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-    func goLocation(latitude latitudeValue: CLLocationDegrees, longitude longitudeValue: CLLocationDegrees, delta span :Double){
+    func goLocation(latitude latitudeValue: CLLocationDegrees, longitude longitudeValue: CLLocationDegrees, delta span :Double) -> CLLocationCoordinate2D{
         let pLocation = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
         let spanValue = MKCoordinateSpanMake(span,span)
         let pRegion = MKCoordinateRegionMake(pLocation,spanValue)
         myMap.setRegion(pRegion, animated: true)
+        
+        return pLocation
+    }
+    
+    func setAnnotation(latitude latitudeValue:CLLocationDegrees, longitude longitudeValue : CLLocationDegrees, delta span :Double, title strTitle: String, subtitle strSubtitle:String) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = goLocation(latitude: latitudeValue, longitude: longitudeValue, delta: span)
+        annotation.title = strTitle
+        annotation.subtitle = strSubtitle
+        myMap.addAnnotation(annotation)
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -63,7 +73,23 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
     }
     
     @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
-        
+        if sender.selectedSegmentIndex == 0 {
+            self.lblLocationInfo1.text = ""
+            self.lblLocationInfo2.text = ""
+            locationManager.startUpdatingLocation()
+        }else if sender.selectedSegmentIndex == 1 {
+            setAnnotation(latitude: 37.4480065, longitude: 126.6553179, delta: 1, title: "인하공업전문대학", subtitle: "인천광역시 남구 인하로 100")
+            self.lblLocationInfo1.text = "보고 계신 위치"
+            self.lblLocationInfo2.text = "인하공업전문대학"
+        }else if sender.selectedSegmentIndex == 2 {
+            setAnnotation(latitude: 37.5307871, longitude: 126.6553179, delta: 0.1, title: "이지스퍼블리싱", subtitle: "서울특별시 영등포구 당산로 41길 11")
+            self.lblLocationInfo1.text = "보고 계신 위치"
+            self.lblLocationInfo2.text = "이지스퍼블리싱 출판사"
+        }else if sender.selectedSegmentIndex == 3 {
+            setAnnotation(latitude: 37.456311, longitude: 126.638735, delta: 0.001, title: "우리 집", subtitle: "인천광역시 남구 능해길46번길 51")
+            self.lblLocationInfo1.text = "보고 계신 위치"
+            self.lblLocationInfo2.text = "삼익아파트"
+        }
     }
 
 }
